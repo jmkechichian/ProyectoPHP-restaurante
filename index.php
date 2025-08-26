3f6c09b81e874a2f9032ada99e0381c7121f0ac8
unknown <jmkechichian@gmail.com>
2025-08-26 19:09:20 -0300

    Actualizando listado chefs, testimonios, footer

--- index.php
+++ index.php
@@ -12,22 +12,116 @@
   <body>
    
     <!-- Banner que ocupa todo el ancho -->
-    <div class="banner">
-        <div class="banner-content">
-            <h1>¡Bienvenido!</h1>
-            <p>Este banner ocupa todo el ancho de la pantalla sin importar el tamaño del dispositivo</p>
-            <button href="#" type="button" class="btn btn-outline-light btn-lg">Ver Menu</button>
-        </div>
-    </div>
+    <section>
+      <div class="banner">
+          <div class="banner-content">
+              <h1>¡Bienvenido!</h1>
+              <p>Este banner ocupa todo el ancho de la pantalla sin importar el tamaño del dispositivo</p>
+              <button href="#" type="button" class="btn btn-outline-light btn-lg">Ver Menu</button>
+          </div>
+      </div>
+  </section>
 
-    <!-- Contenido normal de la página -->
-    <div class="content">
-        <h2>Contenido de la página</h2>
-        <p>Este es el contenido normal de la página que tiene un ancho máximo y está centrado. El banner de arriba se extiende por todo el ancho de la pantalla.</p>
-        <p>Puedes redimensionar la ventana del navegador para ver cómo el banner siempre ocupa el 100% del ancho disponible.</p>
-    </div>
+    <br>
+    <br>
 
-  
+    
+<!-- SECCION CHEFS-->
+
+<section id="chefs "class="container md-4 text-center">
+        <h2>Nuestros Chefs</h2>
+        <p>Listado de nuestros chefs.</p>
+        <div class="row">
+        <div class="col-md-4">
+          <div class="card">
+            <img src="../restaurante/images/chef-1.jpg" class="card-img-top" alt="Chef 1">
+              <div class="card-body">
+                <h5 class="card-title">Chef Juan</h5>
+                <p class="card-text">Especialista en cocina italiana.</p>
+                <div class="social-icons">
+                  <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  <a href="#"><i class="fab fa-twitter"></i></a>
+                  <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+              </div>
+          </div>
+    </div>
+    <div class="col-md-4">
+          <div class="card">
+            <img src="../restaurante/images/chef-2.jpg" class="card-img-top" alt="Chef 2">
+              <div class="card-body">
+                <h5 class="card-title">Chef Felipe</h5>
+                <p class="card-text">Especialista en cocina mexicana.</p>
+                <div class="social-icons">
+                  <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  <a href="#"><i class="fab fa-twitter"></i></a>
+                  <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+              </div>
+          </div>
+    </div>
+    <div class="col-md-4">
+          <div class="card">
+            <img src="../restaurante/images/chef-3.jpg" class="card-img-top" alt="Chef 3">
+              <div class="card-body">
+                <h5 class="card-title">Chef Mati</h5>
+                <p class="card-text">Especialista en cocina japonesa.</p>
+                <div class="social-icons">
+                  <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  <a href="#"><i class="fab fa-twitter"></i></a>
+                  <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+              </div>
+          </div>
+    </div>
+</div>
+
+</section>
+
+    <br>
+    <br>
+
+
+<!-- SECCION TESTIMONIOS-->
+<section id="testiomonios" class="bg-light py-5">
+        <div class="container">
+        <h2 class="text-center mb-4">Testimonios</h2>
+          <div class="row">
+            <div class="col-md-6 d-flex">
+              <div class="card mb-4 w-100">
+                <div class="card-body">
+                  <p class="card-text">"La comida es deliciosa y el servicio es excelente. ¡Volveré pronto!"</p>
+                </div>
+                <div class="card-footer text-muted">
+                  - María López
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 d-flex">
+              <div class="card mb-4 w-100">
+                <div class="card-body">
+                  <p class="card-text">"La comida es deliciosa y el servicio es excelente. ¡Volveré pronto!"</p>
+                </div>
+                <div class="card-footer text-muted">
+                  - María López
+                </div>
+              </div>
+            </div>
+          </div>
+</section>
+<!-- SECCION MENU EL VIDEO ESTA CORTADO EN MIN 30-->
+<section>
+  <h2 class="text-center mb-4">Menu</h2>
+
+
+
+</section>
+
+<footer class="bg-dark text-white text-center py-3">
+        <p>&copy; Proyecto PHP 2025 Tecnologo Informatica.</p>
+</footer>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
   </body>
 </html>
