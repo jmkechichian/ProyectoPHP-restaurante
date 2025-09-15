e89305712406af823dff6b4b573b66415e960cf6
Juan <104105383+jmkechichian@users.noreply.github.com>
2025-09-15 09:32:56 -0300

    Registo y Login Usuario
    Se creo el registro y el login para usuarios admin y clientes

---
+++ cliente/index.php
@@ -0,0 +1,30 @@
+   <?php
+// index.php
+$pageTitle = "Panel de Control Usuario";
+include '../templates/header.php';
+?>
+
+
+<br>
+<br>
+
+<div class="card">
+  <div class="card-header">
+</div>
+  <div class="card-body">
+    <h4 class="card-title">Panel del Usuario</h4>
+    <p class="card-text">Este es el panel principal del Usuario</p>
+</div>
+    <div class="card-footer text-muted">
+        Footer
+</div>
+
+</div>
+
+
+
+<?php
+include '../templates/footer.php';
+
+?>
+   
